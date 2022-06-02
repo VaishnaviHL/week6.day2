0ca70bc431bc9ac1bc6f0c757a39add3b01469f1
VaishnaviHL <103865856+VaishnaviHL@users.noreply.github.com>
2022-06-02 09:47:08 +0530

    Add files via upload

---
+++ CreateLead.java
@@ -0,0 +1,32 @@
+package week6.day2;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class CreateLead extends ProjectSpecificMethods {
+
+	@BeforeTest
+	public void setFileName() {
+		fileName="tc001";
+	}
+
+	@Test(dataProvider="testData")
+
+	public void runCreateLead(String username, String password, String cname, String fname, String lname){
+		driver.findElement(By.id("username")).sendKeys(username);
+		driver.findElement(By.id("password")).sendKeys(password);
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys(cname);
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys(fname);
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys(lname);
+		driver.findElement(By.name("submitButton")).click();
+	}
+
+}
